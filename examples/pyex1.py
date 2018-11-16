f4a49730d3594262627ac27bd5a44176db44bb22
aschenbach <aschenbach@gmail.com>
2018-11-15 19:17:39 -0500

    Created a new file with sample python code.

---
+++ examples/pyex1.py
@@ -0,0 +1,18 @@
+# Author  : freeman
+# Date    : 2018.11.16
+# Version : 0.0.1
+# Desc    : Program 1
+#
+###################################################################
+
+import os
+import sys
+import time
+
+
+def main():
+    print "Hello Planet!"
+
+
+if __name__ == "__main__":
+    main()
