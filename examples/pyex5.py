2e3c2f91418df969a869a52cd6d03ed0553bd334
aschenbach <aschenbach@gmail.com>
2018-11-19 19:57:47 -0500

    added more functions

--- examples/pyex5.py
+++ examples/pyex5.py
@@ -18,21 +18,26 @@ def addTwoNums1():
     result = a + b
     return result
 
+
 def addTwoNums2(a,b):
 
     return float(a + b)
+
 
 def powwow(base,exp):
 
     return math.pow(base,exp)
 
+
 def splitStringBySpace(a):
     
     return a.split(' ') 
 
+
 def splitStringByComma(a):
 
     return a.split(',')
+
 
 def specialFunc():
 
@@ -40,10 +45,12 @@ def specialFunc():
     ans = [(lambda x : x*2)(x) for x in num_list]
     return ans
 
+
 def countItems1():
 
     a = ["mary", "had" , "a", "sheep"]
     print len(a)
+
 
 def countItems2():
 
@@ -55,10 +62,28 @@ def countItems2():
     print "the number of items in this list ist: " + str(cnt)
 
 
+def testOddEven():
+
+    even = []
+    odd = []
+    for i in range(0,200):
+        if i % 2 == 0:
+            even.append(i)
+        else:
+            odd.append(i)
+
+    return even, odd
+
+
 def main():
 
     a = specialFunc()
     print a
+
+    e, o = testOddEven()
+    print "Even list: " + str(e)
+    print "Odd list: " + str(o)
+
 
 if __name__ == "__main__":
     main()
