6680c2843dc411628914fa584deb57d442f86de6
freemanbach <flo@radford.edu>
2018-11-24 16:01:48 -0500

    added code to count text in test.data file

---
+++ examples/pyex7.py
@@ -0,0 +1,31 @@
+# Author  : freeman
+# Date    : 2018.11.24
+# Version : 0.0.1
+# Desc    : Program 7
+#
+###################################################################
+
+import os
+import sys
+import time
+
+
+def main():
+
+    f = open("test.data", "r")
+    lst, cnt = [], 0
+    for y in f:
+        a = y.rstrip('\r\n')
+        a = a.split(" ")
+        for z in a:
+            cnt += len(z)
+            if z != "":
+                lst.append(z)
+            else:
+                continue   
+    print str(lst)
+    print "the numbere of chars ist: " + str(cnt)
+
+
+if __name__ == "__main__":
+    main()
