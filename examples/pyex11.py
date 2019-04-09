f4e42b0708d1565c5ae76adc915f79d24da891a6
Freeman Lo <flo@radford.edu>
2019-04-09 10:20:52 -0400

    Simple Python Error handling example

--- examples/pyex11.py
+++ examples/pyex11.py
@@ -8,5 +8,22 @@
 import os
 import sys
 
+def test():
+    mylst = [ 'b', 0, 2 ]
+    for i in mylst:
+        try:
+            print "The value is: " + str(i)
+            r = float(1.00 / int(i))
+            break
+        except:
+            print "Error: " + str(sys.exc_info()[0]) + " happened."
+            print "Trying next value"
+
+    print "The remainder of: " + str(i) + " " + " is " + str(r)
+
+def main():
+    test()
+
+
 if __name__ == "__main__":
     main()
