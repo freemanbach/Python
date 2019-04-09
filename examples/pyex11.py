15005967614788bd17ba8d07c000e851dc3d38a0
Freeman Lo <flo@radford.edu>
2019-04-09 09:54:14 -0400

    Add this simple python error handling example

---
+++ examples/pyex11.py
@@ -0,0 +1,12 @@
+# Author  : freeman
+# Date    : 2019.04.09
+# Version : 0.0.1
+# Desc    : Program 11
+#         : python Error handling -- simple Error handling example
+###################################################################
+
+import os
+import sys
+
+if __name__ == "__main__":
+    main()
