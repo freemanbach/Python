8370edf8c855af9606b2e32cc60ee26494db8bec
Freeman Lo <flo@radford.edu>
2019-04-27 02:36:03 -0400

    new py3 hello world

---
+++ python3/examples/py3ex1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+# Author  : freeman
+# Date    : 2019.04.26
+# Version : 0.0.1
+# Desc    : Program 1
+#
+###################################################################
+
+import os
+import sys
+import time
+
+
+def main():
+    print("Hello Planet!")
+
+
+if __name__ == "__main__":
+    main()
+
