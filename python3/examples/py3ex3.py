95996dc2c8861fbcbc87f75cc86729827fa9f68d
Freeman Lo <flo@radford.edu>
2019-04-30 11:07:24 -0400

    python3 prompt example NEED Quotes when entering string

---
+++ python3/examples/py3ex3.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# Author  : freeman
+# Date    : 2019.05.01
+# Version : 0.0.1
+# Desc    : Program 3
+#
+###################################################################
+
+import os
+import sys
+import time
+
+
+def main():
+    # must enter in quotes in prompt:  "4 + 3 ="
+    ui = str(input("Enter in a equation with equal symbol at the end: "))
+    values = list(ui.strip().split())
+    if values[len(values)-1] == '=':
+        #print(values)
+        if values[1] == '+':
+            print(float(values[0]) + float(values[2]))
+        elif values[1] == '-':
+            print(float(values[0]) - float(values[2]))
+        elif values[1] == '*':
+            print(float(values[0]) * float(values[2]))
+        elif values[1] == '/':
+            print(float(values[0]) / float(values[2]))
+        else:
+            print("i dont know")
+    else:
+        print("Must have an equal Symbol in the end of your math expression!!")
+
+
+if __name__ == "__main__":
+    main()
