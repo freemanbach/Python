e806e223e0b0594bf5def2faf77eb9e06192ca72
Freeman Lo <flo@radford.edu>
2019-05-10 20:43:20 -0400

    Added a file to parse Mutual Fund Tickers

---
+++ python2/examples/stock/nasdaqMF.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+
+# Author  : freeman
+# Date    : 2019.05.09
+# Version : 0.0.1
+# Desc    : Financial mutual fund Program
+#         : Pull NASDAQ Trader Mutual funds ticker tags to a file
+#         : 
+###################################################################
+
+
+import warnings
+import sys
+import urllib2
+import socket
+
+
+def testConn():
+    try:
+        socket.create_connection(("www.google.com", 80))
+        return True
+    except OSError as err:
+        print "No Connection: " + err.errno
+    return False
+
+
+def retrieveFile():
+    url = 'ftp://ftp.nasdaqtrader.com/symboldirectory/mfundslist.txt'
+    f = urllib2.urlopen(url)
+    data = f.readlines()
+
+    return data
+
+
+def processData(rData):
+
+    mfsymbols = []
+    print "Pocessing Mutual Funds Symbols."
+    for z in range(1, len(rData)-1):
+        mfsymbols.append(rData[z].split('|')[0].strip()) 
+    print "Processing Mutual Funds Symbols: Done."
+
+    return mfsymbols
+
+
+def writeToFile(data):
+    fo = open("mutualFunds.txt", "w")
+    for i in data:
+        fo.write(i+"\n")
+    fo.close()
+
+
+def main():
+    val = int(testConn())
+    if val == 1:
+        print "Internet Connection is ok."
+        dt = retrieveFile()
+        s = processData(dt)
+        writeToFile(s)
+        print
+        print "Completed, there should be a file called mutualFunds.txt"
+    else:
+        print "Your local Internet Connection is down or something wrong with router."
+        print "Reboot Router, Switch, Hub or PC or All four ."
+
+
+if __name__ == "__main__":
+    main()
