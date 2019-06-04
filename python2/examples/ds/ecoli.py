64087aedfa02be7aa141a9dc6ac9d1f7d499ca5d
Freeman Lo <flo@radford.edu>
2019-06-04 16:21:59 -0400

    added a Data Science task for testing

---
+++ python2/examples/ds/ecoli.py
@@ -0,0 +1,26 @@
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+# Load dataset
+url = "ecoli2.data"
+names = ['SeqName', 'MCG', 'GVH', 'LIP', 'CHG', 'AAC', 'ALM1', 'ALM2', 'Class']
+dataset = pandas.read_csv(url, names=names)
+
+print dataset.shape
+print dataset.head(10)
+print dataset.describe()
+print dataset.groupby('Class').size()
+########################################################
+dataset.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+plt.show()
