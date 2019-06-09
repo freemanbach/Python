142f2acca01775b18c3b9ca297459e2f4581e919
Freeman Lo <flo@radford.edu>
2019-06-09 13:56:37 -0400

    added some title madness

--- python2/examples/ds/ecoli.py
+++ python2/examples/ds/ecoli.py
@@ -1,3 +1,4 @@
+# libs madness
 import pandas
 from pandas.plotting import scatter_matrix
 import matplotlib.pyplot as plt
@@ -17,15 +18,22 @@ url = "ecoli2.data"
 names = ['SeqName', 'MCG', 'GVH', 'LIP', 'CHG', 'AAC', 'ALM1', 'ALM2', 'Class']
 dataset = pandas.read_csv(url, names=names)
 
+# printing out some statistical madness
 print dataset.shape
 print dataset.head(10)
 print dataset.describe()
 print dataset.groupby('Class').size()
-########################################################
-dataset.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+
+# box and whisker plot
+dataset.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False, title='Box Plot Ecoli')
 plt.show()
+
+# Histogram plot
 dataset.hist()
+plt.suptitle("Ecoli histogram")
 plt.show()
-# this graph doesnt work unless the above four lines are commented out.
+
+# Super matrix plot.
 scatter_matrix(dataset)
+plt.suptitle("Super Matrix")
 plt.show()
