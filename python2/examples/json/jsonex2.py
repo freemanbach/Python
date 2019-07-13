7e757f3a84081a648965d757bc387b65506c5e1a
Freeman <flo@radford.edu>
2019-07-13 15:07:02 -0400

    Parse Alpha json file

---
+++ python2/examples/json/jsonex2.py
@@ -0,0 +1,53 @@
+# Author  : freeman
+# Date    : 2019.07.15
+# Version : 0.0.1
+# Desc    : Program 1
+#         : Parsing json data from alphaadvantage.co
+#         : Parsing these json files seems to be a bit tricky
+###################################################################
+
+
+import json
+import sys
+
+"""
+Dictionary inside another dictionary
+{Date; {1: Open: value; 2: High: value; 3: Low: value; 4: Close: value; 5: Volume: value}}
+"""
+def processItems(fdsa):
+
+    openditem, highditem, lowditem, closeditem, volditem = {}, {}, {}, {}, {}
+
+    for k, v in fdsa.iteritems():
+        #dateitem.append(str(k).strip())
+        x = str(k).strip()
+        for a, b in v.iteritems():
+            if str(a).strip() == "4. close":
+                closeditem[x] = str(b).strip()
+            if str(a).strip() == "1. open":
+                openditem[x] = str(b).strip()
+            if str(a).strip() == "2. high":
+               highditem[x] = str(b).strip()
+            if str(a).strip() == "3. low":
+                lowditem[x] = str(b).strip()
+            if str(a).strip() == "5. volume":
+                volditem[x] = str(b).strip()
+
+    for o, p in openditem.iteritems():
+        print o + "  " + p
+
+
+def readJson():
+    with open('aaplAlpha.json', 'r') as fi:
+        ds = json.load(fi)
+        tds = ds['TimeSeries']
+    return tds
+
+
+def main():
+    tmp = readJson()
+    processItems(tmp)
+
+
+if __name__ == "__main__":
+    main()
