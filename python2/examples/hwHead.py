716b010dcfd864c6d0bcfb268a7dba65f5cc744e
Freeman <flo@radford.edu>
2019-08-13 17:58:10 -0400

    added header example info

---
+++ python2/examples/hwHead.py
@@ -0,0 +1,6 @@
+# Author      : freeman
+# Date        : 2019.08.08
+# Version     : 0.0.1
+# Assignment  : HW # 
+# Description : In-Class Example
+#################################################
