7d11f560070971a638da6d908234b0aa8cf7d15c
freemanbach <flo@radford.edu>
2020-02-18 20:03:00 -0500

    Added NoneType as result

--- python3/examples/unittest/grade.py
+++ python3/examples/unittest/grade.py
@@ -5,7 +5,7 @@ import sys
 
 # if a is {int||float}  -> solution 
 # if b is {int||float}  -> solution 
-# if a || b is {String} -> None
+# if a || b is {String} -> NoneType
 def add(a,b):
     """
     Exception: except NameError outside of this function
@@ -14,33 +14,33 @@ def add(a,b):
     if str(type(a)).split(' ')[1].split('\'')[1].lower() == 'int' or str(type(a)).split(' ')[1].split('\'')[1].lower() == 'float' and str(type(b)).split(' ')[1].split('\'')[1].lower() == 'int' or str(type(b)).split(' ')[1].split('\'')[1].lower() == 'float':
         ans = a+b
         return ans
-    return 'None'
+    return None
 
 # if a is {int||float}  -> solution 
 # if b is {int||float}  -> solution 
-# if a || b is {String} -> None
+# if a || b is {String} -> NoneType
 def sub(a,b):
     """
     Exception: except NameError outside of this function
     """
     if str(type(a)).split(' ')[1].split('\'')[1].lower() == 'int' or str(type(a)).split(' ')[1].split('\'')[1].lower() == 'float' and str(type(b)).split(' ')[1].split('\'')[1].lower() == 'int' or str(type(b)).split(' ')[1].split('\'')[1].lower() == 'float':
         return a+b
-    return 'None'
+    return None
 
 
 # if a is {int||float}  -> solution 
 # if b is {int||float}  -> solution 
-# if a || b is {String} -> None
+# if a || b is {String} -> NoneType
 def mul(a,b):
     """
     Exception: except NameError outside of this function
     """
     if str(type(a)).split(' ')[1].split('\'')[1].lower() == 'int' or str(type(a)).split(' ')[1].split('\'')[1].lower() == 'float' and str(type(b)).split(' ')[1].split('\'')[1].lower() == 'int' or str(type(b)).split(' ')[1].split('\'')[1].lower() == 'float':
         return a+b
-    return 'None'
+    return None
 
 
-# if divisor is 0 -> NoneType
+# if divisor is 0 -> 'division by zero'
 # if divisor is not 0 -> floatValue
 def div(a,b):
     z = 0
