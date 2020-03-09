7cc3b34e422d0b10ba95c9998a87bce9847f3947
freemanbach <flo@radford.edu>
2020-03-09 17:48:20 -0400

    added support for JSON with words

--- python3/examples/hashcode/hashwords.py
+++ python3/examples/hashcode/hashwords.py
@@ -11,8 +11,7 @@ use       : python3 hashwords.py words_alpha.txt
 
 
 import hashlib  # main hashing libs 
-import hmac     # message algorithm
-import zlib     # adler and crc32
+import json     # import Json
 import sys
 
 
@@ -38,7 +37,13 @@ def processwords():
             
     return words
 
-
+"""
+data = {}
+data['vocabs'] = []
+data['vocabs'].append( { 'word': v[0], 'sha1': v[1], 'sha256': v[2]  } )
+data = {'people':[{'name': 'Scott', 'website': 'stackabuse.com', 'from': 'Nebraska'}]}
+ 
+"""
 def processhash(mylst):
     """ prints the hashed values to screen. """ 
     hashword, tmp = dict(), []
@@ -49,13 +54,23 @@ def processhash(mylst):
         h_sha256 = hashlib.sha256(a.encode('utf-8')).hexdigest()
         h_sha384 = hashlib.sha384(a.encode('utf-8')).hexdigest()
         # left this out intentionally
-        #h_sha512 = hashlib.sha512(a.encode('utf-8')).hexdigest()
-        hashword.update( { i : [h_sha1, h_sha256, h_sha384 ] } )
+        # h_sha512 = hashlib.sha512(a.encode('utf-8')).hexdigest()
+        hashword.update( { i : [ h_sha1, h_sha256, h_sha384 ] } )
     
     # Printing out the hashes(sha1, sha384, sha512) for each word
     # from the word file, you can also add h_sha512 to the list
-    for k, v in hashword.items():
-        print( k, "  " , v )
+    #for k, v in hashword.items():
+    #    print( k, "  " , v[0] )
+    createJson(hashword)
+    
+
+def createJson(ddict):
+    jdata = {}
+    jdata['vocabs'] = []
+    for k, v in ddict.items():
+        jdata['vocabs'].append( { 'word': str(k), 'sha1': str(v[0]), 'sha256': str(v[1]),'sha384': str(v[2]) } )
+    jd = json.dumps(jdata, sort_keys=False, indent=4)
+    print(jd)
 
     
 def main():
