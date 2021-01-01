f4ac7fac7d406290b41c7cb4a1075e674e0bfcd7
freemanbach <flo@radford.edu>
2021-01-01 15:32:36 -0500

    altered ticker

--- python3/mariadb/processcompany.py
+++ python3/mariadb/processcompany.py
@@ -5,11 +5,12 @@ author     : freeman
 date       : 20200101
 desc       : Process Nasdaq Stock Data into sql for company table
            : 
-use        : python3 processnasdaq.py ns.csv companyData.sql
+use        : python3 processcompany.py ns.csv companyData.sql
            :
 """
 
 import sys
+import random
 
 def processdata(item, output):
     data = []
@@ -18,7 +19,6 @@ def processdata(item, output):
         for i in f:
             line = i.split(',')
             z = line[0].strip().lower()
-            z = z.replace(".", "")
             z = z.replace("/", "")
             
             y = line[9].strip().lower()
@@ -56,12 +56,29 @@ def processdata(item, output):
         for i in data:
             f.write(i)
 
+def getsymbols():
+    symbols, tmp = [], []
+    with open( "ns.csv", "r") as f:
+        for i in f:
+            a = str(i.split(",")[0]).strip().lower()
+            a = a.replace(".", "")
+            a = a.replace("/", "")
+            tmp.append(str(a)+"\n")
+    for i in list(range(0,10)):
+        symbols.append(random.choice(tmp))
+
+    print(symbols)
+    with open("symbols.txt", "w") as f:
+        for i in symbols:
+            f.write(str(i))
+
 
 def main():
     if len(sys.argv) == 3:
         dfile = sys.argv[1]
         ofile = sys.argv[2]
         processdata(str(dfile), str(ofile))
+        getsymbols()
     else:
         print("Not enough parameters.")
         print("See Header Information.")
