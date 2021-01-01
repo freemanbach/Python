8dd2fae079726c7d2d4a7ac4eeee332dfbfab081
freemanbach <flo@radford.edu>
2021-01-01 16:05:53 -0500

    updated this DB

--- python3/mariadb/simpleFinance.sql
+++ python3/mariadb/simpleFinance.sql
@@ -1,4 +1,10 @@
 
+-- DB design
+-- CI/CD process of this DB
+
+-- KISS ACID
+-- https://database.guide/what-is-acid-in-databases/
+
 drop database if exists simplefinance;
 create database if not exists simplefinance;
 USE simplefinance;
@@ -16,11 +22,21 @@ CREATE TABLE `company` (
                            PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
+
+drop table if exists `ticker`;
+CREATE TABLE `ticker` (
+                            `id` int(11) not null auto_increment,
+                            `ticker_id` int(10) NULL,
+                            `ticker` varchar(10) NULL,
+                            PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
+
 drop table if exists `daily_price`;
 CREATE TABLE `daily_price` (
                           `id` int(11) not null auto_increment,
                           `ticker_id` int(10) NULL,
-                          `ticker` varchar(10) NULL,
                           `date` date NULL,
                           `time` timestamp NULL,
                           `open` decimal(12,4)  NULL,
@@ -30,18 +46,17 @@ CREATE TABLE `daily_price` (
                           `volume` decimal(12,4) NULL,
                           PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-alter table daily_price add constraint fk_daily_price_company foreign key (`ticker_id`) references company(`id`) on update cascade on delete cascade;
+-- alter table daily_price add constraint fk_daily_price_company foreign key (`ticker_id`) references company(`id`) on update cascade on delete cascade;
 
 
 drop table if exists `stock_adjust`;
 CREATE TABLE `stock_adjust` (
-                                    `id` int(11) not null auto_increment,
-                                    `ticker_id` int(10) NULL,
-                                    `ticker` varchar(10) NULL,
-                                    `date` date NULL,
-                                    `factor` decimal(12,4)  NULL,
-                                    `dividend` decimal(12,4)  NULL,
-                                    `split_ratio` decimal(12,4) NULL,
-                                    PRIMARY KEY (id)
+                            `id` int(11) not null auto_increment,
+                            `ticker_id` int(10) NULL,
+                            `date` date NULL,
+                            `factor` decimal(12,4)  NULL,
+                            `dividend` decimal(12,4)  NULL,
+                            `split_ratio` decimal(12,4) NULL,
+                            PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-alter table stock_adjust add constraint fk_stock_adjust_company foreign key (`ticker_id`) references company(`id`) on update cascade on delete cascade;
+-- alter table stock_adjust add constraint fk_stock_adjust_company foreign key (`ticker_id`) references company(`id`) on update cascade on delete cascade;
