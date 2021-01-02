fa91ef48edb272ebdec9a26d57dfb812a3d02722
freemanbach <flo@radford.edu>
2021-01-01 20:22:20 -0500

    decided to scrap nasdaq and use alphavantage instead

--- python3/mariadb/simpleFinance.sql
+++ python3/mariadb/simpleFinance.sql
@@ -9,20 +9,26 @@ drop database if exists simplefinance;
 create database if not exists simplefinance;
 USE simplefinance;
 
--- nasdaq screen app
--- https://www.nasdaq.com/market-activity/stocks/screener
+
+-- alphavantage.co (Overview partly)
+-- https://www.alphavantage.co/documentation/
+-- https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
 drop table if exists `company`;
 CREATE TABLE `company` (
                            `id` int(10) not null auto_increment,
+                           `ticker` varchar(10) NULL
+                           `assettype` varchar(50)  NULL,
                            `name` varchar(100)  NULL,
-                           `ticker` varchar(10) NULL,
                            `description` varchar(1000) NULL ,
+                           `exchange` varchar(10) NULL,
+                           `currency` varchar(10) NULL,
+                           `country` varchar(10) NULL,
                            `sector` varchar(100) NULL ,
                            `industry` varchar(200) NULL,
                            PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
-
+-- 
 drop table if exists `ticker`;
 CREATE TABLE `ticker` (
                             `id` int(11) not null auto_increment,
@@ -32,7 +38,7 @@ CREATE TABLE `ticker` (
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 
-
+-- timestamp,open,high,low,close,volume
 drop table if exists `daily_price`;
 CREATE TABLE `daily_price` (
                           `id` int(11) not null auto_increment,
@@ -49,14 +55,32 @@ CREATE TABLE `daily_price` (
 -- alter table daily_price add constraint fk_daily_price_company foreign key (`ticker_id`) references company(`id`) on update cascade on delete cascade;
 
 
-drop table if exists `stock_adjust`;
-CREATE TABLE `stock_adjust` (
+
+-- alphavantage.co (Overview partly)
+-- https://www.alphavantage.co/documentation/
+-- https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
+drop table if exists `stock_overview`;
+CREATE TABLE `stock_overview` (
                             `id` int(11) not null auto_increment,
                             `ticker_id` int(10) NULL,
                             `date` date NULL,
-                            `factor` decimal(12,4)  NULL,
+                            `PEratio` decimal(12,4)  NULL,
+                            `PEGratio`  decimal(12,4) NULL,
+                            `bookvalue` decimal(12,4) NULL,
+                            `dividendpershare` decimal(12,4) NULL,
+                            `dividendyield` decimal(12,4) NULL,
+                            `EPS` decimal(12,4) NULL,
+                            `profitmargin` decimal(12,4) NULL,
+                            `quarterlyearningsgrowthYOY` decimal(12,4) NULL,
+                            `quarterlyrevenuegrowthYOY` decimal(12,4) NULL,
+                            `returnonequityTTM` decimal(12,4) NULL,
+                            `revenueTTM` decimal(12,4) NULL,
+                            `grossprofitTTM` decimal(12,4) NULL,
+                            `beta` decimal(12,4) NULL,
+                            `52weekhigh` decimal(12,4) NULL,
+                            `52weeklow` decimal(12,4) NULL,
                             `dividend` decimal(12,4)  NULL,
-                            `split_ratio` decimal(12,4) NULL,
+                            `payoutratio` decimal(12,4) NULL,
                             PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 -- alter table stock_adjust add constraint fk_stock_adjust_company foreign key (`ticker_id`) references company(`id`) on update cascade on delete cascade;
