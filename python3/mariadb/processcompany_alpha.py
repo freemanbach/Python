4a5fbbdb6d1ede7925b44ebadf078d8c7df137ad
freemanbach <flo@radford.edu>
2021-01-02 01:27:21 -0500

    Alpha Vantage stock company overview

---
+++ python3/mariadb/processcompany_alpha.py
@@ -0,0 +1,58 @@
+#!/bin/env python3
+
+"""
+author     : freeman
+date       : 20200102
+desc       : Process Alpha Vantage Stock Data into sql for our company table
+           : 
+use        : python3 processcompany_alpha.py Stock_Symbol.json data.sql
+use        : python3 processcompany_alpha.py ibm.json data.sql
+           :
+DATA       : https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=YOUR_PERSONAL_API_KEY
+           : 
+READ       : First download the JSON file using your web-browser, copy and paste the json data onto a
+           : text editor and save the file as STOCK_SYMBOL.json as shown in the use area. 
+           : Then run this python script to generate the single line of sql as in data.sql file. 
+           : mysql -u YOUR_USER_NAME -p < data.sql
+"""
+
+
+import sys
+import random
+import json
+
+def processdata(infile, otfile):
+    sql, a = [], ""
+    with open(infile, 'r') as jf:
+        data = json.load(jf)
+    z = str(data['Description']).strip()
+    z = z.replace("'", "")
+    a = "INSERT INTO company (ticker, assettype, name, description, exchange,currency ,country ,sector ,industry ) VALUES" \
+             "(" + "\'" +str(data['Symbol']).strip() + "\'" +  ","   \
+                + "\'" + str(data['AssetType']).strip() + "\'" + "," \
+                + "\'" + str(data['Name']).strip() + "\'" + "," \
+                + "\'" + z + "\'" + "," \
+                + "\'" + str(data['Exchange']).strip() + "\'" + "," \
+                + "\'" + str(data['Currency']).strip() + "\'" + "," \
+                + "\'" + str(data['Country']).strip() + "\'" + "," \
+                + "\'" + str(data['Sector']).strip() + "\'" + "," \
+                + "\'" + str(data['Industry']).strip() + "\'"  ");" + "\n" 
+    sql.append("use simplefinance;\n")
+    sql.append(a)
+    print(sql)
+    
+    with open(otfile, 'w') as f:
+        for i in sql:
+            f.write(i)
+
+
+def main():
+    if len(sys.argv) == 3:
+        filename = sys.argv[1]
+        outfile = sys.argv[2]
+        processdata(str(filename).strip(), str(outfile).strip())
+    else:
+        print("Not enough parameters.")
+
+if __name__ == "__main__":
+    main()
