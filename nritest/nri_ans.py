5156a5dfb8a0e199b9a8040f873087195a10a003
freemanbach <flo@radford.edu>
2021-03-21 14:41:19 -0400

    Init check in of code

---
+++ nritest/nri_ans.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# author         : Freeman
+# date           : 2021.03.21
+# desc           : Parsing number of questions
+# version        : 0.0.1
+################################################
+
+import sys
+import time
+import random
+
+
+def main():
+
+    if len(sys.argv) == 2:
+        
+    else:
+        print("not enough parameters")
+        sys.exit(0)
+
+
+
+if __name__ == "__main__":
+    main()
