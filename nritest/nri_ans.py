bfcc9ef766d9b5550d46af14652fabcb668dd812
freemanbach <flo@radford.edu>
2021-03-21 14:46:29 -0400

    added more namespace for backup

--- nritest/nri_ans.py
+++ nritest/nri_ans.py
@@ -9,14 +9,22 @@
 import sys
 import time
 import random
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def main():
 
-    if len(sys.argv) == 2:
-        
-    else:
+    if len(sys.argv) > 2:
+        print("Too many parameters")
+        print("python nri_ans NumQuestions")
+        sys.exit(0)
+    elif if len(sys.argv) == 2:
+
+    elif len(sys.argv) < 2:
         print("not enough parameters")
+        print("python nri_ans NumQuestions")
         sys.exit(0)
 
 
