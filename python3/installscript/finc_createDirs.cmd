e0d63a3bba9872361503bb592b4d08ab5a6ebf69
freemanbach <flo@radford.edu>
2021-05-07 01:52:41 -0400

    finc students

---
+++ python3/installscript/finc_createDirs.cmd
@@ -0,0 +1,14 @@
+@echo OFF
+:: 
+cls
+:: Section 1: Create Dirs
+echo.============================
+echo. Creating Directories
+echo.============================
+echo.
+::
+cd 
+mkdir %userprofile%\finc3984
+mkdir %userprofile%\finc3984\hws
+mkdir %userprofile%\finc3984\labs
+echo."Done"
