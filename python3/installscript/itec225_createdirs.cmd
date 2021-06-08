c0f24fae9f67123ffe43863cf16c088ba6c9b02e
freemanbach <flo@radford.edu>
2021-06-08 12:05:24 -0400

    create dirs

---
+++ python3/installscript/itec225_createdirs.cmd
@@ -0,0 +1,20 @@
+@echo OFF
+:: 
+cls
+:: Section 1: Create Dirs
+echo.============================
+echo. Creating Directories
+echo.============================
+echo.
+::
+cd 
+mkdir %userprofile%\itec225
+mkdir %userprofile%\itec225\hws
+mkdir %userprofile%\itec225\essay
+mkdir %userprofile%\itec225\projects
+mkdir %userprofile%\itec225\projects\project1
+mkdir %userprofile%\itec225\projects\project2
+mkdir %userprofile%\itec225\projects\project3
+mkdir %userprofile%\itec225\projects\project4
+mkdir %userprofile%\itec225\projects\project5
+echo."Done"
