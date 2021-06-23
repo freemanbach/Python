a8af456c65babe50a07ef49af2c53784663bdbfd
freeman lo <flo@radford.edu>
2021-06-22 20:34:42 -0400

    added itec109 create dirs

---
+++ python3/installscript/itec109_createdirs.cmd
@@ -0,0 +1,14 @@
+@echo OFF
+:: 
+cls
+:: Section 1: Create Dirs
+echo.============================
+echo. Creating Directories
+echo.============================
+echo.
+::
+cd 
+mkdir %userprofile%\itec109
+mkdir %userprofile%\itec109\hws
+mkdir %userprofile%\itec109\labs
+echo."Done"
