bb756cc9e8757a4f3a1550965edf1fdf3a0fd6a3
freeman lo <flo@radford.edu>
2021-12-16 13:37:01 -0800

    added auto dirs creation.

---
+++ python3/installscript/finc_createDirs_win.cmd
@@ -0,0 +1,14 @@
+@echo OFF
+:: 
+cls
+:: Section 1: Create Dirs
+echo.============================
+echo. Creating Directories
+echo.============================
+echo.
+::
+cd 
+mkdir %userprofile%\finc3114
+mkdir %userprofile%\finc3114\hws
+mkdir %userprofile%\finc3114\labs
+echo."Done"
